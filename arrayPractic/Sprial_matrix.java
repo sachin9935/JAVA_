dcfdeeecb7b53f1ae5c179e3a70f3e9a34a2d41e
Sachin Mishra <sm979657@gmail.com>
2024-09-02 21:09:22 +0530

    idea

---
+++ arrayPractic/Sprial_matrix.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sprial_matrix {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int ar[][] = new int[r][c];
+    }
+    
+}
