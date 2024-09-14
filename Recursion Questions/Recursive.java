431aba1bfaf543d66ddcf990a3428b428e71f4c0
Sachin Mishra <sm979657@gmail.com>
2024-09-14 13:20:59 +0530

    solving questions

---
+++ Recursion Questions/Recursive.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Recursive{
+    static int SumOf_naturalNo(int n){
+        if(n==0) return 0;
+        return SumOf_naturalNo(n-1)+n;
+    }
+
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(SumOf_naturalNo(n));
+
+        sc.close();
+    }
+}
