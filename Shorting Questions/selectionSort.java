979bf2f20c4907b9e2a50ed697f944b390928886
Sachin Mishra <sm979657@gmail.com>
2024-11-23 15:43:12 +0530

    Update selectionSort.java

--- Shorting Questions/selectionSort.java
+++ Shorting Questions/selectionSort.java
@@ -10,15 +10,18 @@ class selectionSort {
                 }
             }
             // swap the elements
+            if(max_index!=i){
             int temp = arr[i];
             arr[i] = arr[max_index];
             arr[max_index] = temp;
+            }
+            
 
         }
     }
 
     public static void main(String args[]) {
-        int arr[] = { 5, 4, 3, 2, 1 };
+        int arr[] = {1,2,3,4};
         Selection_sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i] + " ");
